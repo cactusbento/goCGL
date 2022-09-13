e25c30f5f0715b93fe2d1a1fa3686d4e0b2a1fcb
CactusBento <cactusbento@outlook.com>
2022-09-13 16:20:16 -0400

    tcell added, basic loop implemented

---
+++ init.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+)
+
+func InitScreen() tcell.Screen {
+	s, err := tcell.NewScreen()
+	if err != nil { panic(err) }
+
+	if err := s.Init(); err != nil {
+		panic(err)
+	}
+
+	s.EnableMouse()
+
+	return s
+}
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gdamore/tcell/v2"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Conway's Game Of Life")
+	
+	s := InitScreen()
+
+	// Set default text style
+	DefStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	s.SetStyle(DefStyle)
+
+	quit := func() {
+		s.Fini()
+		os.Exit(0)
+	}
+
+	x := 0
+
+	
+	go func() {
+		for {
+			ev := s.PollEvent()
+
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyCtrlC {
+					quit()
+				}
+			}
+		}
+	}()
+
+	for {
+		s.Clear()
+
+
+		s.SetContent(x, 0, '=', nil, DefStyle)
+		x++
+
+		if w, _ := s.Size(); x >= w - 1 {
+			quit()
+		}
+
+		s.Show()
+		time.Sleep(time.Millisecond * 100)
+	}
 }
